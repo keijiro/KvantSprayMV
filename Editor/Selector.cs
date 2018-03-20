47b63cb4d54dd4e616d979b10e74b2eaebf554d1
Keijiro Takahashi <keijiro@gmail.com>
2018-03-20 22:38:56 +0900

    UPM package branch

---
+++ Editor/Selector.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+
+namespace Kvant
+{
+    static class SprayMVSelector
+    {
+        [MenuItem("Packages/Kvant SprayMV")]
+        static void OpenPackageDirectory()
+        {
+            var path = "Packages/jp.keijiro.kvant.spray-mv/README.md";
+            Selection.activeObject = AssetDatabase.LoadMainAssetAtPath(path);
+            EditorGUIUtility.PingObject(Selection.activeObject);
+        }
+    }
+}
